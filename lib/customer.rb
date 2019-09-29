16c849e2ab8299cba3382104972e5735abf39ba1
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-29 03:14:38 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,8 +4,15 @@ class Customer
   end
   
   def meals
-  Meal.all.select do |meal|
-    meal.customer == self
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
   end
-end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
 end
